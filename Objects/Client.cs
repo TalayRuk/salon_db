39c0c183046601b150f621d5bfe6ac8ca9f3dcf4
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-29 10:42:33 -0700

    add getall, fix deleteall for client, and complete db object

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -42,15 +42,47 @@ namespace Salon
 
     public string GetFullName()
     {
-      name = _first_name + " " + _last_name;
+      string name = _first_name + " " + _last_name;
       return name;
     }
 
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientFirstName = rdr.GetString(1);
+        string clientLastName = rdr.GetString(2);
+
+        Client newClient = new Client(clientFirstName, clientLastName, clientId);
+        allClients.Add(newClient);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allClients;
+    }
+
     public static void DeleteAll()
-    SqlConnection conn = DB.Connection();
-    conn.Open();
-    SqlCommand cmd = new SqlCommand("DELETE FROM clients;", conn);
-    cmd.ExecuteNonQuery;
-    conn.Close();
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM clients;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
   }
 }
