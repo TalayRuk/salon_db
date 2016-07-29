bd1458232a2816b55681aee16e321bb69f9dc807
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-29 15:12:18 -0700

    add routes and views for adding a stylist class to DB

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace Salon
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["index.cshtml", allStylists];
+      };
+      Post["/stylist/new"] = _ => {
+        Stylist newStylist = new Stylist( Request.Form["new-stylist-first"],
+                                          Request.Form["new-stylist-last"],
+                                          Request.Form["new-stylist-expertise"]);
+        newStylist.Save();
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["index.cshtml", allStylists];
+      };
+    }
+  }
+}
