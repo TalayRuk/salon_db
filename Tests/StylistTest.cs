f24742049e53e236bb21ce882e2a0b35ef370c76
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-29 10:58:07 -0700

    add Test1 for stylist (confirm DB empty)

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -1,20 +1,28 @@
-// using Xunit;
-// using System.Collections.Generic;
-// using System;
-// using System.Data;
-// using System.Data.SqlClient;
-//
-// namespace Salon
-// {
-//   public class StylistTest : IDisposable
-//   {
-//     public StylistTest()
-//     {
-//       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
-//     }
-//     public void Dispose()
-//     {
-//       Stylist.DeleteAll();
-//     }
-//   }
-// }
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Salon
+{
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+
+    [Fact]
+    public void T1_DBEmptyAtFirst()
+    {
+      int result = Stylist.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+  }
+}
